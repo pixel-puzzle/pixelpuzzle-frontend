77eea929fd9f6b9177d0fca8439d94a7e3605a29
sunxuan <1406243464@qq.com>
2021-09-15 17:57:12 +0800

    add color

--- src/config/nft.js
+++ src/config/nft.js
@@ -2,10 +2,12 @@ const nftConfig = [
   {
     name: 'helmet-duck',
     count: 29,//三角形个数
+    bgColor: '#C09DA7', // 背景色
   },
   {
     name: 'pixelpuzzle',
     count: 46,//三角形个数
+    bgColor: '#1A1A1D', // 背景色
   }
 ]
 
